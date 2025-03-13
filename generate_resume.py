193793bfd46c8b878c9926cab218d237a1051427
Ayaan Ahmad Siddiqui <40334511+ayaan2525@users.noreply.github.com>
2025-03-13 23:34:04 +0100

    Update generate_resume.py

--- generate_resume.py
+++ generate_resume.py
@@ -3,7 +3,7 @@ from jinja2 import Environment, FileSystemLoader
 
 # load yaml data
 with open(".github/workflows/config.yaml", "r") as file:
-    resume_data = yanl.safe_load(file)
+    resume_data = yaml.safe_load(file)
 
 env = Environment(loader=FileSystemLoader("templates"))
 template = env.get_template("resume_template.html")
